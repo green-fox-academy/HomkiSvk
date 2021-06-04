49eeb413e1084579ed300f344c3d214df951431d
Martin Novosedlik <martin.novosedlik@gmail.com>
2021-06-04 11:05:41 +0200

    DrawDiamond upload

---
+++ week-01/day-4/src/com/company/DrawDiamond.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class DrawDiamond {
+    public static void main(String[] args) {
+        // Write a program that reads a number from the standard input, then draws a
+        // diamond like this:
+        //
+        //
+        //    *
+        //   ***
+        //  *****
+        // *******
+        //  *****
+        //   ***
+        //    *
+        //
+        // The diamond should have as many lines as the number was
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("How big diamond u want to see? Give me a number:");
+        int size = scan.nextInt();
+        int spaces = size-1;
+        int signs = 1;
+
+        for (int i = 1; i <= size; i++) {
+            for (int j = 0; j < spaces; j++) {
+                System.out.print(' ');
+            }
+            spaces--;
+            for (int k = 0; k < signs; k++) {
+                System.out.print('*');
+            }
+            signs+=2;
+            System.out.println();
+        }
+        spaces+=2;
+        signs-=4;
+        for (int i = 1; i < size; i++) {
+            for (int j = 0; j < spaces; j++) {
+                System.out.print(' ');
+            }
+            spaces++;
+            for (int k = 0; k < signs; k++) {
+                System.out.print('*');
+            }
+            signs -= 2;
+            System.out.println();
+        }
+    }
+}
