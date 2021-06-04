22cdd7c2239631574b50d8a2bbb5a72579a4c250
Martin Novosedlik <martin.novosedlik@gmail.com>
2021-06-04 11:18:50 +0200

    DrawSquare upload

---
+++ week-01/day-4/src/com/company/DrawSquare.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class DrawSquare {
+    public static void main(String[] args) {
+        // Write a program that reads a number from the standard input, then draws a
+        // square like this:
+        //
+        //
+        // %%%%%%
+        // %    %
+        // %    %
+        // %    %
+        // %    %
+        // %%%%%%
+        //
+        // The square should have as many lines as the number was
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("How big square u want to see? Give me a number:");
+        int size = scan.nextInt();
+
+        for (int i = 0; i < size; i++) {
+            if (i==0 || i==size-1) {
+                for (int j = 0; j < size; j++) {
+                    System.out.print('%');
+                }
+                System.out.println();
+            } else {
+                System.out.print('%');
+                for (int j = 0; j < size-2; j++) {
+                    System.out.print(' ');
+                }
+                System.out.println('%');
+            }
+
+        }
+
+    }
+}
