45be37c864ec529f81088843168a1f9fdb1eada7
Martin Novosedlik <martin.novosedlik@gmail.com>
2021-06-04 10:49:30 +0200

    DrawPyramid upload

---
+++ week-01/day-4/src/com/company/DrawPyramid.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class DrawPyramid {
+    public static void main(String[] args) {
+        // Write a program that reads a number from the standard input, then draws a
+        // pyramid like this:
+        //
+        //
+        //    *
+        //   ***
+        //  *****
+        // *******
+        //
+        // The pyramid should have as many lines as the number was
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("How big pyramid u want to see? Give me a number:");
+        int size = scan.nextInt();
+        int spaces = size-1;
+        int signs = 1;
+
+        for (int i = 1; i <= size; i++) {
+            for (int j = 0; j < spaces; j++) {
+                System.out.print(' ');
+            }
+            spaces--;
+            for (int k = 0; k < signs; k++) {
+                System.out.print('*');
+            }
+            signs+=2;
+            System.out.println();
+        }
+
+    }
+}
