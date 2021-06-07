1ad6300e04c0c291b128cd2afe80ba97338d087f
Martin Novosedlik <martin.novosedlik@gmail.com>
2021-06-07 15:28:29 +0200

    Greet upload

---
+++ week-02/day-1/src/com/company/Greet.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Greet {
+    //  From now on, create the usual class wrapper
+    //  and main method on your own
+
+    // - Create a string variable named `al` and assign the value `Green Fox` to it
+    // - Create a function called `greet()` that greets it's input parameter
+    //     - Greeting is printing e.g. `Greetings dear Green Fox`
+    // - Greet `al`
+    public static String greet(String name){
+        return "Greetings dear " + name;
+    }
+
+    public static void main(String[] args) {
+        String al = "Green Fox";
+        System.out.println(greet(al));
+    }
+}
