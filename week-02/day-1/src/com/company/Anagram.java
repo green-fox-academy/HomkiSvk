62b0e4f4c8b5cf5824672afddf5d1bcd5395d569
Martin Novosedlik <martin.novosedlik@gmail.com>
2021-06-07 20:58:42 +0200

    anagram upload

--- week-02/day-1/src/com/company/Anagram.java
+++ week-02/day-1/src/com/company/Anagram.java
@@ -1,13 +1,9 @@
 package com.company;
 
-
-
 public class Anagram {
 
     public static boolean isAnagram (String a, String b){
         boolean status ;
-
-
         if (a.length() == b.length()){
             char[] aChars = a.toCharArray();
             for (int i = 0; i < aChars.length/2; i++) {
@@ -15,16 +11,15 @@ public class Anagram {
                 temp = aChars[i];
                 aChars[i] = aChars[aChars.length-1 -i];
                 aChars[aChars.length-1 -i] = temp;
-
             } String c = new String(aChars);
-
             status = c.equalsIgnoreCase(b);
         } else status = false;
-
         return status;
     }
 
     public static void main(String[] args) {
-        System.out.println(isAnagram("dogie", "god"));
+        System.out.println("dog and god: " +isAnagram("dog", "god"));
+
+        System.out.println("green and fox: " +isAnagram("green", "fox"));
     }
 }
