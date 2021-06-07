22a58308aa5d17d38aaf4f7cbc9afcf47a8e229f
Martin Novosedlik <martin.novosedlik@gmail.com>
2021-06-07 13:18:29 +0200

    Third upload

---
+++ week-02/day-1/src/com/company/Third.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Third {
+    // - Create an array variable named `numbers`
+    //   with the following content: `[4, 5, 6, 7]`
+    // - Print the third element of `numbers`
+
+    public static void main(String[] args) {
+        int[] numbers = {4, 5, 6, 7};
+        System.out.println("From array" + Arrays.toString(numbers) + " 3rd element is: " +numbers[2]);
+    }
+}
