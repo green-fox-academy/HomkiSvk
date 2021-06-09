6102cde52a64b36c2363bbe5557a54c5078d9f7c
Martin Novosedlik <martin.novosedlik@gmail.com>
2021-06-09 11:51:42 +0200

    WriteSingleLine  + my-file.txt upload

---
+++ week-02/day-3/src/com/company/WriteSingleLine.java
@@ -0,0 +1,30 @@
+package com.company;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.Scanner;
+
+public class WriteSingleLine {
+    // Write a function that is able to manipulate a file
+    // by writing your name into it as a single line.
+    // The file should be named "my-file.txt".
+    // In case the program is unable to write the file,
+    // it should print the following error message: "Unable to write file: my-file.txt".
+
+    public static void main(String[] args) {
+        Scanner scan =new Scanner(System.in);
+
+        System.out.println("What's your name?");
+        String name = scan.next();
+
+        Path filePath = Paths.get("my-file.txt");
+
+        try {
+            Files.writeString(filePath, name);
+        } catch (IOException e) {
+            System.out.println("Unable to write file: my-file.txt");
+        }
+
+    }
+}
