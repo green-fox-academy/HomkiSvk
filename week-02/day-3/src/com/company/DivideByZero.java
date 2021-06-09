ba48056b3d52ea4386452a6c82d022ca9320fbba
Martin Novosedlik <martin.novosedlik@gmail.com>
2021-06-09 11:24:46 +0200

    DivideByZero upload

---
+++ week-02/day-3/src/com/company/DivideByZero.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class DivideByZero {
+    // Create a function that divides number 10
+    // by a number that's passed as a paramater and prints the result.
+    // It should print "fail" if the parameter is 0
+
+    private static void divide (int a){
+        try {
+            int result = 10 / a;
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Can't divide by zero!");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give me a divisor: ");
+        int divisor = scanner.nextInt();
+
+        divide(divisor);
+    }
+}
