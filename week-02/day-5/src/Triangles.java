c4029052d454bf2197e2197db34ed68bb2caef4d
Martin Novosedlik <martin.novosedlik@gmail.com>
2021-06-11 00:23:13 +0200

    Triangles cleanup

--- week-02/day-5/src/Triangles.java
+++ week-02/day-5/src/Triangles.java
@@ -8,26 +8,25 @@ public class Triangles {
         Graphics2D g2 = (Graphics2D) graphics;
         g2.setStroke(new BasicStroke(2));
 
-        int count = 30;                // only this parameter needs to change to modify number of triangles in a row
+        int count = 20;                // only this parameter needs to change to modify number of triangles in a row
         int size = WIDTH / count;
         int positionX = 0;
         int positionY = HEIGHT - 50;
         double height = (size * (Math.sqrt(3)) / 2);
 
         for (int j = count; j > 0; j--) {
-            drawTriangle(positionX, positionY, size, graphics);
+            drawTriangle(positionX, positionY, size, height, graphics);
             int inlineX = positionX;
             for (int k = j - 1; k > 0; k--) {
                 inlineX += size;
-                drawTriangle(inlineX, positionY, size, graphics);
+                drawTriangle(inlineX, positionY, size, height, graphics);
             }
             positionX += size / 2;
             positionY -= height;
         }
     }
 
-    private static void drawTriangle(int a, int b, int size, Graphics graphics) {
-        double height = (size * (Math.sqrt(3)) / 2);
+    private static void drawTriangle(int a, int b, int size, double height, Graphics graphics) {
         int[] xpoints = {a, a + size, a + size / 2};
         int[] ypoints = {b, b, b - ((int) height)};
         graphics.drawPolygon(xpoints, ypoints, xpoints.length);
