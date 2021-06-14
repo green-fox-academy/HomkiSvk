8b1a99ca90758604d93a4466c70f77851354ed75
Martin Novosedlik <martin.novosedlik@gmail.com>
2021-06-14 11:18:07 +0200

    NumberAdder upload

--- week-03/day-1/src/NumberAdder.java
+++ week-03/day-1/src/NumberAdder.java
@@ -1,2 +1,19 @@
 public class NumberAdder {
+    // Write a recursive function that takes one parameter: n and
+    // adds numbers from 1 to n.
+
+    public static void main(String[] args) {
+
+        System.out.println(numAdd(-7));
+    }
+
+    private static int numAdd(int i) {
+        if (i==0)
+            return 0;
+        if (i < 0)
+            return i + numAdd(i+1);
+        if (i == 1)
+            return 1;
+        return i + numAdd(i-1);
+    }
 }
