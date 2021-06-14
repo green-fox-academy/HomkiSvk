77763e116f1e52c6565eb44c1ad868579d132f09
Martin Novosedlik <martin.novosedlik@gmail.com>
2021-06-14 11:45:58 +0200

    BunniesAgain upload

---
+++ week-03/day-1/src/BunniesAgain.java
@@ -0,0 +1,25 @@
+public class BunniesAgain {
+    // We have bunnies standing in a line,
+    // numbered 1, 2, 3, 4, ...
+    // The odd bunnies (1, 3, ...) have the normal 2 ears.
+    // The even bunnies (2, 4, ..) we'll say have 3 ears,
+    // because they each have a raised foot.
+    // Recursively return the number of "ears" in
+    // the bunny line 1, 2, ... n (without loops or multiplication).
+
+    public static void main(String[] args) {
+
+        System.out.println(oddEvenBunnyEars(9));
+
+    }
+
+    private static int oddEvenBunnyEars(int i) {
+        if (i==1)
+            return 2;
+        if (i%2==0)
+            return 3 + oddEvenBunnyEars(i-1);
+        else
+            return 2 + oddEvenBunnyEars(i-1);
+    }
+
+}
