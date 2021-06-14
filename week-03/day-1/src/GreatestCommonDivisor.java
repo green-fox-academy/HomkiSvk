0ae4b5aa6ef214c977c955d616adc3ef5a0f5ade
Martin Novosedlik <martin.novosedlik@gmail.com>
2021-06-14 14:55:47 +0200

    MaximumFinder upload

---
+++ week-03/day-1/src/GreatestCommonDivisor.java
@@ -0,0 +1,13 @@
+public class GreatestCommonDivisor {
+    // Find the greatest common divisor of two numbers using recursion.
+
+    public static void main(String[] args) {
+
+        //System.out.println(gretDivisor(34, 29));
+
+    }
+
+    /*private static int gretDivisor(int a, int b) {
+        if ()
+    }*/
+}
