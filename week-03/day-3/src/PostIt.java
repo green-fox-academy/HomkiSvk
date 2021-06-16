831c47c4c93790714a9c06a286e7a823ccf59afa
Martin Novosedlik <martin.novosedlik@gmail.com>
2021-06-16 16:14:16 +0200

    BlogPost and PostIt upload

--- week-03/day-3/src/PostIt.java
+++ week-03/day-3/src/PostIt.java
@@ -1,2 +1,24 @@
 public class PostIt {
+
+    private String backgroundColor;
+    private String text;
+    private String textColor;
+
+    PostIt(String bgColor, String text, String textColor){
+        this.backgroundColor = bgColor;
+        this.text = text;
+        this.textColor = textColor;
+    }
+
+
+    public static void main(String[] args) {
+
+        PostIt orange = new PostIt("orange", "Idea 1", "blue");
+        PostIt pink = new PostIt("pink", "Awesome", "black");
+        PostIt yellow = new PostIt("yellow", "Superb", "green");
+
+
+    }
+
 }
+
