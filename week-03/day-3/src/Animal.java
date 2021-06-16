34ceda96303273df73ae23799b92302560b1e3a0
Martin Novosedlik <martin.novosedlik@gmail.com>
2021-06-16 16:28:48 +0200

    Animal upload

---
+++ week-03/day-3/src/Animal.java
@@ -0,0 +1,23 @@
+public class Animal {
+
+    private int hunger;
+    private int thirst;
+
+    public Animal() {
+        this.hunger = 50;
+        this.thirst = 50;
+    }
+
+    public void eat() {
+        this.hunger--;
+    }
+
+    public void drink() {
+        this.thirst--;
+    }
+
+    public void play() {
+        this.hunger++;
+        this.thirst++;
+    }
+}
