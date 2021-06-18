af4295828ba8450b923d3d1a50df3fc51382b870
Martin Novosedlik <martin.novosedlik@gmail.com>
2021-06-18 12:46:59 +0200

    Anagram reworked

--- week-02/day-1/src/com/company/Anagram.java
+++ week-02/day-1/src/com/company/Anagram.java
@@ -1,25 +1,22 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Anagram {
 
-    public static boolean isAnagram (String a, String b){
-        boolean status ;
-        if (a.length() == b.length()){
-            char[] aChars = a.toCharArray();
-            for (int i = 0; i < aChars.length/2; i++) {
-                char temp;
-                temp = aChars[i];
-                aChars[i] = aChars[aChars.length-1 -i];
-                aChars[aChars.length-1 -i] = temp;
-            } String c = new String(aChars);
-            status = c.equalsIgnoreCase(b);
-        } else status = false;
-        return status;
-    }
+    public static boolean isAnagram(String a, String b) {
+        if (a.length() == b.length()) {
+            char[] wordA = a.toCharArray();
+            char[] wordB = b.toCharArray();
+            Arrays.sort(wordA);
+            Arrays.sort(wordB);
 
-    public static void main(String[] args) {
-        System.out.println("dog and god: " +isAnagram("dog", "god"));
+            return Arrays.equals(wordA, wordB);
+        }
 
-        System.out.println("green and fox: " +isAnagram("green", "fox"));
+        public static void main (String[]args){
+            System.out.println("dog and god: " + isAnagram("dog", "god"));
+
+            System.out.println("green and fox: " + isAnagram("green", "fox"));
+        }
     }
-}
