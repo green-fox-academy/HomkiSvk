f9764f09a4ccab3ca255e94e6cd88f2ed7e2bef7
Martin Novosedlik <martin.novosedlik@gmail.com>
2021-06-22 14:01:33 +0200

    comparable upload

---
+++ week-04/src/day2/comparable/Domino.java
@@ -0,0 +1,34 @@
+package day2.comparable;
+
+public class Domino implements Comparable<Domino> {
+    private final int left;
+    private final int right;
+
+    public Domino(int left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public int getLeftSide() {
+        return left;
+    }
+
+    public int getRightSide() {
+        return right;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + left + ", " + right + "]";
+    }
+
+    @Override
+    public int compareTo(Domino o) {
+        if (this.left > o.left || ((this.left == o.left) && (this.right > o.right))) {
+            return 1;
+        } else if (this.left < o.left || ((this.left == o.left) && (this.right < o.right))) {
+            return -1;
+        } else
+            return 0;
+    }
+}
