71a6daebe047b3d8dcb16a1ea40a1fedcbdea0e2
Martin Novosedlik <martin.novosedlik@gmail.com>
2021-07-15 11:22:13 +0200

    initial push from bash

---
+++ week06/libraryHomework/src/main/java/com/greenfoxacademy/libraryhomework/LibraryHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.libraryhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryHomeworkApplication.class, args);
+    }
+
+}
---
+++ week06/libraryHomework/src/test/java/com/greenfoxacademy/libraryhomework/LibraryHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.libraryhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
