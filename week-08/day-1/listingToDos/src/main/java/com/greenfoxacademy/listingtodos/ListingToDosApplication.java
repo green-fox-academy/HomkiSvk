914fa63bdf310fe72eb8ce00d60fd5f9bc3187c7
Martin Novosedlik <martin.novosedlik@gmail.com>
2021-07-26 10:56:24 +0200

    todos init

---
+++ week-08/day-1/listingToDos/src/main/java/com/greenfoxacademy/listingtodos/ListingToDosApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.listingtodos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListingToDosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListingToDosApplication.class, args);
+    }
+
+}
